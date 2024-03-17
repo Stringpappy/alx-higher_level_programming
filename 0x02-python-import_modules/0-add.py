65a1dce8a7e2ddb4cb2b3815974fa21576b203f7
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-17 13:54:00 -0700

    2-argv

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0.py
-a = 1
-b = 2
-sum = add_0.add(a, b)
-print("{} + {} = {}".format(a,b,sum)
+if __name__ == "__main__":
+    import add_0
+    a = 1
+    b = 2
+    sumit = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, sumit))
