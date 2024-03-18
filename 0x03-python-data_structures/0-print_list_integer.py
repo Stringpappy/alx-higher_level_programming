a7b8bbade9246e626d0744dc3e314d120e636794
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-18 07:34:33 -0700

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/ur/bin/python3
 def print_list_integer(my_list=[]):
-    listnum = len(my_list)
-    for count in range(listnum):
-            print("{}".format(my_list[count]))
+    for count in my_list:
+        print("{:d}".format(count))
