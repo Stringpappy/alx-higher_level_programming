9a7e26d5b7bd19568ae78a072b7b72394218d93d
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-18 07:02:50 -0700

    addnum

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     import add_0
     a = 1
     b = 2
-    sumit = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, sumit))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
