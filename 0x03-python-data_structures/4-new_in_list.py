6e13f4f642b1327d6af6616683a3393c96a9c3a4
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-18 14:50:56 -0700

    turple

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    pat = my_list[:]
+    if idx < 0:
+        return pat
+    if idx > len(my_list) -1:
+        return pat
+    pat[idx] = element
+    return pat
