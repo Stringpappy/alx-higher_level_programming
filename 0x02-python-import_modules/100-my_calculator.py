68257873fc4cfaea4c78231622fc921511c94399
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-19 13:53:27 -0700

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,22 @@
-#!/bin/python3
-if__name__ == "__main__":
+#!/usr/bin/python3
+if __name__ == "__main__":
     from calculator_1 import sub, add, mul, div
     from sys import argv
     argnum = len(argv)
     if argnum != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        a = int(argv[1])
-        operator = argv[2]
-        b = int(argv[3])
-        if operator == '+' ||operator == '-' || operator == '*'  || operator == '/':
-            if operator = '+':
-            print(f"{a} {operator} {b} = {add(a, b)}")
-        elif operator == '-':
-            print(f"{a} {operator} {b} = {sub(a, b)}")
-        elif operator == '*':
-            print(f"{a} {operator} {b} = mul(a, b)}")
-        elif operator == '/':
-            print(f"{a} {operator} {b} = {div(a, b)}")
-        else:
-            print("Unknown operator .Available operators: +, -, *, and /"
-            exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if operator == '+':
+        print(f"{a} {operator} {b} = {add(a, b)}")
+    elif operator == '-':
+        print(f"{a} {operator} {b} = {sub(a, b)}")
+    elif operator == '*':
+       print(f"{a} {operator} {b} = {mul(a, b)}")
+    elif operator == '/':
+       print(f"{a} {operator} {b} = {div(a, b)}")
+    else:
+        print("Unknown operator . Available operators: +, -, *, and /")
+        exit(1)
