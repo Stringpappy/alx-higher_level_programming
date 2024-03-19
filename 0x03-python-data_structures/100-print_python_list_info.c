8fd629ee21433f36d4dd3b6867a27f7134ede4bb
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-18 22:49:09 -0700

    how python works

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,9 +2,10 @@
 #include <object.h>
 #include <listobject.h>
 void print_python_list_info(PyObject *p)
+{
 	long int size = PyList_Size(p);
 	int co;
-	PyListObject *obj = (PyList *)p;
+	PyListObject *obj = (PyListObject *)p;
 
 	printf("[*] size of the python list = %li\n", size);
 	print("[*] Allocated = %li\n", obj->allocated);
