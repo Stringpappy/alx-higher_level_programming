b8dd284672a68fcf22e61a7efe3f43736ff54617
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-19 10:19:02 -0700

    Infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     addnum = 0
-    argnum = len(argv)
-    for count in range(1, argnum):
+    arglen = len(argv)
+    for count in range(1, arglen):
         addnum += int(argv[count])
     print("{}".format(addnum))
