9afa0d618e02c8885b3511237f2af2e4a44dcc24
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-19 12:53:40 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    somfunc = dir()
-    funclen = len(somefunc)
-    for count in range (0, funclen):
+    somefunc = dir()
+    for count in range (0,len(somefunc)):
         if somefunc[count][:2] != "__":
             print("{:s}".format(somefunc[count]))
