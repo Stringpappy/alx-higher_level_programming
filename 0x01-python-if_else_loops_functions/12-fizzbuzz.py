1f659ff83a647dca16c3c3690cf1e05b29b53041
stringpappy <jejeodunayo70@gmail.com>
2024-03-29 23:00:59 +0100

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz")
         elif number % 5 == 0:
             print("Buzz")
-        elif number % 5 and number % 3 == 0:
+        if number % 15 || number % 15 * 2 == 0 || number % 15 * 3 || number % 15 * 4 || number % 15 * 6:
             print("FizzBuzz")
         else :
             print(number)
