21ba1ed9d691379baeb4b5a68751f3106e43b14d
stringpappy <jejeodunayo70@gmail.com>
2024-04-02 16:19:09 -0700

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,8 @@ def safe_function(fct, *args):
     try:
         outcome = fct(*args)
         return outcome
-    except (ZeroDivisionError, IndexError, ValueError,TypeError):
+    except ZeroDivisionError:
+        pass
+    except (IndexError, ValueError, TypeError):
         print("Exception: {}".format(sys.ecx_info()[1]), file=sys.stderr)
     return None
