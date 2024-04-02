ca29fabf66b21e3617f1f44e1e3dfc07e55f3c84
stringpappy <jejeodunayo70@gmail.com>
2024-04-02 15:51:11 -0700

    exception

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        outcome = fct(*args)
+        return outcome
+    except (ZeroDivisionError, IndexError, ValueError,TypeError):
+        print("Exception: {}".format(sys.ecx_info()[1]), file=sys.stderr)
+    return None
