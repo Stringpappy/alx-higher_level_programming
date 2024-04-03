8871dfe3b817f87b7f166906cf3e4de3312f3cff
stringpappy <jejeodunayo70@gmail.com>
2024-04-03 15:35:58 +0100

    empty square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
 class Square:
-    """emty class of a square"""
+    """empty class of a square."""
     pass
-
-    def square():
-        pass
