a5b954b38affd32d39bceb56eedc2d71294890ef
stringpappy <jejeodunayo70@gmail.com>
2024-04-10 02:17:05 -0700

    more class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,34 +3,32 @@
 
 
 class Rectangle:
-    """super class of width and height"""
-    def __init__(self, width=0, height=0):
-        """initializing a nnew rectangle """
-        self.width = width
+    """the class rectangle with empty instance and field"""
+    def __nit__(self, width =0, height=0):
+        self.width = width 
         self.height = height
-
+    
     @property
     def width(self):
-        """to set ractangle width"""
+        """ set the rectangle width """
         return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
     @property
     def height(self):
-        """to set rectangle height"""
+        """ set the rectangle width """
         return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+        if width < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
