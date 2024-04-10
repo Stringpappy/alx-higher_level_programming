2fed2d8d35e9fa2c9bbfa3536afbfc072d03ddb6
stringpappy <jejeodunayo70@gmail.com>
2024-04-09 20:11:23 -0700

    everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """class LockedClass with no class or object attribute"""
 
+
 class LockedClass:
-    """dstop the user from ynamically creating new instance attributes, 
+    """dstop the user from ynamically creating new instance attributes,
     except if the new instance called first_name"""
 
     __slots__ = ["first_name"]
-
