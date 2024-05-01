7cd2b92f0264cb3ff61708f02e96c538f2085df3
stringpappy <jejeodunayo70@gmail.com>
2024-04-30 19:28:53 -0700

    suare

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").MyClass.__doc__)')"""
+"""an empty suare"""
+
 
 class Square:
-    """Define a square"""
-    
-    def __init__ (self, size):
-        """A new square"""
-        self.__size == size
+    """empty suare"""
+
+    def __init__(self, size):
+        """initiallising"""
+        self.__size = size
