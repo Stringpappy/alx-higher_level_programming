36a55ae4b86996fe462c4734d9449b725d1a6ed1
stringpappy <jejeodunayo70@gmail.com>
2024-04-30 20:04:27 -0700

    suare

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").MyClass.__doc__)')"""
+"""an empty suare"""
+
+
 class Square:
-    """ define a  square"""
+    """empty suare"""
     def __init__(self, size=0):
-        """initialise  the new squsre"""
-        self.dize = siz3
+        self.__size = size
+        """intialise new square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
-        @property
-        def size(self):
-            """the size of the square"""
-            return (self.__size)
+    def area(self):
+        """public function """
+        return (self.__size * self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
+    @property
+    def size(self):
+        """the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
             self.__size = value
 
         def area(self):
