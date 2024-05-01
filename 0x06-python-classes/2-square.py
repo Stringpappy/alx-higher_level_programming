fc71b7d785587dc140ca8b1985a36b3e3158bd89
stringpappy <jejeodunayo70@gmail.com>
2024-04-30 19:40:28 -0700

    suare

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,12 +4,10 @@
 
 class Square:
     """empty suare"""
-
-
-    def __init__ (self,size=0):
+    def __init__(self, size=0):
         self.__size = size
         """intialise new square"""
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
