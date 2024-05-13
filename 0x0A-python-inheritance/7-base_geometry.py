7501b2b543e494d995c092c88e370359604fa885
stringpappy <jejeodunayo70@gmail.com>
2024-05-13 11:00:46 -0700

    inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class and exception"""
+
+
+class BaseGeometry:
+    """super cass!"""
+    def area(self):
+        """way to compute the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """to validate a value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
