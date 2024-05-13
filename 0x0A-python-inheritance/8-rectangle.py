6cc04ba558a3e661bc500c5e521e9530734cdff5
stringpappy <jejeodunayo70@gmail.com>
2024-05-13 08:33:32 -0700

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,10 +16,10 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """initilising"""
-    if type(value) != int:
-        raise TypeError("{} must be an integer".format(name))
-    if value <= 0:
-        raise ValueError("{} must be greater than 0".format(name))
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
     def __init__(self, width, height):
         """initalising"""
         self.integer_validdator("height", height)
