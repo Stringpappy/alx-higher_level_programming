83152b3df657c73169b1c34f8a74dc53497cb683
stringpappy <jejeodunayo70@gmail.com>
2024-05-13 14:10:09 -0700

    inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" class rectangle that inherite from basegeometry"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""Rectangle class."""
 
-class Rectngle(BaseGeometry):
-    """a subclass of super class basegeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherit from class BaseGeometry."""
+
     def __init__(self, width, height):
-        self.integer_validator("height", height)
+        """Initialize Rectangle with width and height."""
+        self.__width = 0
+        self.__height = 0
         self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-        def area(self):
-            """area of a rectangle"""
-            return self.__width * self.__height
+    def area(self):
+        """area of rect calculation."""
+        return self.__width * self.__height
 
-        def __str__(self):
-            """string rep method"""
-            return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+    def __str__(self):
+        """ string representation of Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
