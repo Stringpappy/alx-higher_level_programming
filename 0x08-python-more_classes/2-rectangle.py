779b23659f581ad909fde77ad68a1c3b777d5475
stringpappy <jejeodunayo70@gmail.com>
2024-05-16 10:17:47 -0700

    more classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,32 +3,46 @@
 
 
 class Rectangle:
-    """the class rectangle with empty instance and field"""
-    def __init__(self, width =0, height=0):
+    """super class of width and height"""
+    def __init__(self, width=0, height=0):
+        """initializing a nnew rectangle """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ set the rectangle width """
+        """to set ractangle width"""
         return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
-        """ set the rectangle width """
+        """to set rectangle height"""
         return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """returns the area of a rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns the perimeter of a rectangle"""
+        if self.__width == 0:
+            return 0
+        else:
+            p = 2 * (self.width + self.height)
+            return p
