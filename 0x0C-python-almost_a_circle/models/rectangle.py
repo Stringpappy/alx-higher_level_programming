9926afd4bb48236338776fc3f4d26a6140c4fc4c
stringpappy <jejeodunayo70@gmail.com>
2024-05-18 09:10:58 -0700

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,8 +21,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """Setter for width attribute"""
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self._width = value
 
     @property
     def height(self):
@@ -32,6 +35,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter height attribute"""
+        if not isinstance(value, int):
+            raise TypeError(f"height must be an integer")
+        if value <= 0:
+            raise ValueError(f"height must be > 0")
         self.__height = value
 
     @property
@@ -42,6 +49,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value <  0:
+            raise ValueError(f"x must be >= 0")
         self.__x = value
 
     @property
@@ -52,4 +63,36 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter y"""
+        if not isinstance(value, int):
+            raise TypeError(f"y must be an integer")
+        if valuee <  0:
+            raise ValueError(f"y must be >= 0")
         self.__y = value
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        for _ in range(self.height):
+            print("#" * self.width)
+     
+     def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height}, x={self.x}, y={self.y})
+        "
+
+    def update(self, *args):
+        """update"""
+    l = len(args)
+    if l >= 1:
+        self.id = args[0]
+    if l >= 2:
+        self.width = args[1]
+    if l >= 3:
+        self.height = args[2]
+    if l >= 4:
+        self.x = args[3]
+    if l >= 5:
+        self.y = args[4]
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
