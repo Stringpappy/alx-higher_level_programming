d4c4501106f9d4a06877e1bd13d46f7896675492
stringpappy <jejeodunayo70@gmail.com>
2024-05-23 14:27:15 -0700

    mysql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --script that insert a new_record in a row in first_table from database 'hbtn_0c_0'
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "School");
+INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
