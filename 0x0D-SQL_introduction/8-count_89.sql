a1be19eeaf0a12f5c52f970a77b0066703d4bf63
stringpappy <jejeodunayo70@gmail.com>
2024-05-23 14:09:38 -0700

    mysql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- script that hat displays the number records with id = 89 in the table 
 --first_table of the database hbtn_0c_0
 
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+SELECT COUNT * FROM first_table WHERE id = 89;
