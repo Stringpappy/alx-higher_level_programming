51489b9a3b58bffb0eaf46eea7cdde00f539d04c
stringpappy <jejeodunayo70@gmail.com>
2024-05-23 14:17:02 -0700

    mysql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,7 @@
 -- Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT score; COUNT(1) AS number FROM second_table;
-GROUP BY score;
+
+SELECT score, COUNT(*) AS number
+FROM hbtn_0c_0.second_table
+GROUP BY score
 ORDER BY number DESC;
+
