0ffb46e14d795ba3cdfff813cb732dc18293c502
stringpappy <jejeodunayo70@gmail.com>
2024-05-23 14:05:44 -0700

    mysql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 --  a script that creates a table second_table in the database hbtni_0c_0
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
 -- table created
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(1, "John", 10);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(2, "Alex", 3);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(3, "Bob", 14);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(4, "George", 8);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(1, “John”, 10);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(2, “Alex”, 3);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(3, “Bob”, 14);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUE(4, “George”, 8);
