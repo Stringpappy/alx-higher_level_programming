02e9bebabbc8f9461fee860553cb82738ef592eb
stringpappy <jejeodunayo70@gmail.com>
2024-06-20 14:29:31 -0700

    javascript object, class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,36 @@
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width === 0 || this.height === 0) {
+      console.log(' ');
+    } else {
+      for (let c = 0; c < this.height; c++) {
+        const column = 'X'.repeat(this.width);
+        console.log(column);
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width !== 0 && this.height !== 0) {
+      const swap = this.width;
+      this.width = this.height;
+      this.height = swap;
+    }
+  }
+
+  double () {
+    if (this.width !== 0 && this.height !== 0) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
