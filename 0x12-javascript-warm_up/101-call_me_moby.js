1250c087a62275c207dea7ac200012ca0ffc940b
stringpappy <jejeodunayo70@gmail.com>
2024-06-19 19:51:05 -0700

    javascript

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* func that exec x time a func */ 
+/* func that exec x time a func */
 exports.callMeMoby = function (x, theFunction) {
   for (let y = 0; y < x; y++) {
     theFunction();
