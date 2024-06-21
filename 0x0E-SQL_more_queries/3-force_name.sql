2271903a97fdc48e19f799ef1bc93095d754c7c5
stringpappy <jejeodunayo70@gmail.com>
2024-06-21 06:32:53 -0700

    mysqql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- a script that creates the table force_name on your MySQL server
+--a script that  creates a table in a database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
