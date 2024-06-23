c3abf63fe5c023d858cf7117fba4bcd8b244dfa6
stringpappy <jejeodunayo70@gmail.com>
2024-06-23 14:59:57 -0700

    sqlachemy

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-""" a script that takes in an argument and displays all
- values in the states table 
-of hbtn_0e_0_usa where name matches the argument
+""" 
+A script that takes in an argument and displays all values 
+in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database_name> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-    mycur = db.cursor()
-    mycur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
-    states = mycur.fetchall()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    for state in states:
-        print(state)
+    try:
+        db = MySQLdb.connect(user=username, passwd=password, db=database, port=3306)
+        cursor = db.cursor()
+
+        query = "SELECT * FROM states WHERE CONVERT(`name` USING Latin1) COLLATE Latin1_General_CS = %s;"
+        cursor.execute(query, (state_name,))
+        
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database:", e)
+        sys.exit(1)
+
+
+
