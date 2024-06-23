b067769fa5f8042b85a0fc22f454317848a39b3c
stringpappy <jejeodunayo70@gmail.com>
2024-06-23 15:05:33 -0700

    sqlachemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" the script that List all cities of a state """
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
 import sys
-import MySQLdb
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
 
-    mycur = db.cursor()
-    mycur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = '{}';".format(sys.argv[4]))
-    states = mycur.fetchall()
+    states = session.query(State).order_by(State.id).all()
 
-    print(", ".join([state[1] for state in states]))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
