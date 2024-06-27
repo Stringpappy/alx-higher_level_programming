57895f2d16f42abdc3e47a7abeb8fb02e82a414f
stringpappy <jejeodunayo70@gmail.com>
2024-06-27 02:49:05 -0700

    model.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""thon file that contains the class definition of a 
-State and an instance Base = declarative_base"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """subclass that defines each city
-    """
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
