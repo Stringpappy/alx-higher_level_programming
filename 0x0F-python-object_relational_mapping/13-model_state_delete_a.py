bb1dc402210da94dfbb5f9ce2e380dc2c4a53c7c
stringpappy <jejeodunayo70@gmail.com>
2024-06-27 04:54:22 -0700

    model.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that deletes all State objects with a name containing the letter a
+"""
+Deletes all State objects with a name containing the letter a
 """
 import sys
 from model_state import Base, State
@@ -11,8 +12,8 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    make_session = sessionmaker(bind=engine)
-    session = make_session()
+    mySession = sessionmaker(bind=engine)
+    session = mySession()
 
     states = session.query(State).filter(State.name.like('%a%')).all()
 
