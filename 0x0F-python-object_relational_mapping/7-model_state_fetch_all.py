2932ddf782882ff6abe6a18b1c70c4f9bfaf8be2
stringpappy <jejeodunayo70@gmail.com>
2024-06-27 04:15:00 -0700

    model.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 """
-script that lists all State objects from the database hbtn_0e_6_usa
+show all State objects from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -12,8 +12,8 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    make_session = sessionmaker(bind=engine)
-    session = make_session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     states = session.query(State).order_by(State.id).all()
 
