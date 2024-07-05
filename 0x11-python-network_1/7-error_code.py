398f26a70d16432b046ae84accd054838917b135
stringpappy <jejeodunayo70@gmail.com>
2024-07-05 09:34:28 -0700

    python network 1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a python script that Sends a POST request to http://0.0.0.0:5000/search_user with a given letter. 
+a python script that Sends a POST request to
+http://0.0.0.0:5000/search_user with a given letter.
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
   - If no letter is provided, sends `q=""`.
