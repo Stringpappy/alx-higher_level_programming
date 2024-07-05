191957667b8e16314baa6daacc9339ca9f41c64d
stringpappy <jejeodunayo70@gmail.com>
2024-07-05 11:44:36 -0700

    python file I/O

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Python task that Defines a class Student."""
+
+
+class Student:
+    """the  super aclss student."""
+
+    def __init__(self, first_name, last_name, age):
+        """to tialize a new Student  attribute
+        Args:
+            first_name (str): The first name of the student.
+            """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to Get a dictionary representation of the Student.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
