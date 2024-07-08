77c311d800135921167b1c847d9f4cc060a359ec
stringpappy <jejeodunayo70@gmail.com>
2024-07-08 15:41:22 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request 
-[ $# -eq 0 ] && { echo "Usage: $0 <URL>"; exit 1; }; curl -s -o /dev/null -w "%{http_code}\n" $1 | grep -q 200 && curl -s $1
+[ $# -eq 0 ] && { echo "Usage: $0 <URL>"; exit 1; }; curl -s -o /dev/null -w "%{http_code}\n" $1 | grep -q 200 && curl -s $1 | curl - sfL "$1" - X GET
