ed985cb9721b3e66010a996358919d640fa4e543
stringpappy <jejeodunayo70@gmail.com>
2024-07-20 01:51:55 -0700

    almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,70 @@
 #!/usr/bin/python3
-"""clss square that inherit fro Revtngle"""
+"""
+module that  contains the "Square" class
+"""
 from models.rectangle import Rectangle
 
-class square(Rectangle):
-    """subclass"""
-    def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
 
-        def __str__(self):
-            """str"""
-            return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+class Square(Rectangle):
+    """func that Represent a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square."""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Update the Square.
+        """
+        if args and len(args) != 0:
+            l = 0
+            for arg in args:
+                if l == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif l == 1:
+                    self.size = arg
+                elif l == 2:
+                    self.x = arg
+                elif l == 3:
+                    self.y = arg
+                l += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, lv in kwargs.items():
+                if key == "id":
+                    if lv is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = lv
+                elif key == "size":
+                    self.size = lv
+                elif key == "x":
+                    self.x = lv
+                elif key == "y":
+                    self.y = lv
+
+    def to_dictionary(self):
+        """ func that Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """func that Return the print() a str representation of a Square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
