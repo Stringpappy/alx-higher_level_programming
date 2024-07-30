2f24e46ab6c17d3e4806af331a4b8812ac4e2784
stringpappy <jejeodunayo70@gmail.com>
2024-07-30 14:50:18 -0700

    js scaping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,16 +3,16 @@ const request = require('request');
 
 request(process.argv[2], function (err, response, body) {
   if (err == null) {
-    const the_rsp = {};
+    const thersp = {};
     const json = JSON.parse(body);
     for (let x = 0; x < json.length; x++) {
       if (json[x].completed === true) {
-        if (the_rsp[json[x].serId] === undefined) {
-          the_rsp[json[x].userId] = 0;
+        if (thersp[json[x].serId] === undefined) {
+          thersp[json[x].userId] = 0;
         }
-        the_rsp[json[x].userId]++;
+        thersp[json[x].userId]++;
       }
     }
-    console.log(the_rsp);
+    console.log(thersp);
   }
 });
