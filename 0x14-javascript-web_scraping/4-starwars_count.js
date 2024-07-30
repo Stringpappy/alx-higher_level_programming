6031560039e8e8e6704d703b930063b36984f76e
stringpappy <jejeodunayo70@gmail.com>
2024-07-30 14:35:18 -0700

    js scaping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const myrequest = require('myrequest');
-myrequest(process.argv[2], function (error, response, body) {
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
+    const myresults = JSON.parse(body).myresults;
+    console.log(myresults.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
