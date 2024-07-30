9225294d606391fe87d73e12e155f335f4458ec7
stringpappy <jejeodunayo70@gmail.com>
2024-07-30 14:20:27 -0700

    javascript scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const my  = require('my');
-my.readFile(process.argv[2], 'utf8', function (error, content) {
+const fs  = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
